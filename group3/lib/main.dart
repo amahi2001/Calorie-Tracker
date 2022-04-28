d5f01d1f1af8241241612e153ccd1bd0adcd435e
chelsea8621 <futuredivergents@gmail.com>
2022-04-28 18:26:05 -0400

    Add code for authentication

--- group3/lib/main.dart
+++ group3/lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+
+import 'auth.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
