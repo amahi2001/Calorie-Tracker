efb13582317a8897d209082e29022646c985c7c9
Xavier Chum <alejandro.chum@gmail.com>
2022-05-04 13:38:04 -0400

    changed tabbar home button

--- group3/lib/main.dart
+++ group3/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 import 'firebase_options.dart';
+
 import 'auth.dart'; //authentication view
 import 'previous_days.dart'; //previous days view
 import 'mainscreen.dart'; //main screen view
